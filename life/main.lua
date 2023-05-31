205cda859b59b3951b7c7c3f34d7b4ecf1e8abb8
JT Archie <jtarchie@gmail.com>
2023-05-30 19:14:32 -0600

    provide some optimizations

--- life/main.lua
+++ life/main.lua
@@ -1,14 +1,16 @@
 local function countNeighborsOnGrid(grid, x, y)
 	local neighbors = 0
 
-	for i = x - 1, x + 1 do
-		if 1 <= i and i <= #grid then
-			for j = y - 1, y + 1 do
-				if 1 <= j and j <= #grid[i] then
-					if grid[i][j] and not (i == x and j == y) then
-						neighbors = neighbors + 1
-					end
-				end
+	-- Define the loop limits adjusted for boundary cells
+	local minX = math.max(1, x - 1)
+	local maxX = math.min(#grid, x + 1)
+	local minY = math.max(1, y - 1)
+	local maxY = math.min(#grid[x], y + 1)
+
+	for i = minX, maxX do
+		for j = minY, maxY do
+			if grid[i][j] and not (i == x and j == y) then
+				neighbors = neighbors + 1
 			end
 		end
 	end
@@ -18,19 +20,18 @@ end
 
 local function NextGrid(grid, callback)
 	local nextGrid = {}
+
 	for i = 1, #grid do
-		nextGrid[i] = {}
+		nextGrid[i] = {} -- Preallocate the table
 
 		for j = 1, #grid[i] do
 			local liveCount = countNeighborsOnGrid(grid, i, j)
 
-			nextGrid[i][j] = false
+			-- Directly assign boolean values based on conditions
+			nextGrid[i][j] = (grid[i][j] and 2 <= liveCount and liveCount <= 3) or (not grid[i][j] and liveCount == 3)
 
-			if (grid[i][j] and 2 <= liveCount and liveCount <= 3) or ((not grid[i][j]) and liveCount == 3) then
-				nextGrid[i][j] = true
-				if callback then
-					callback(i, j, liveCount)
-				end
+			if nextGrid[i][j] and callback then
+				callback(i, j, liveCount)
 			end
 		end
 	end
@@ -40,8 +41,12 @@ end
 
 local grid = {}
 local points = {}
-local pointsCallback = function(x, y, _numNeighbors)
-	table.insert(points, { x - 1, y - 1 })
+local pointsCallback = function(x, y, numNeighbors)
+	if numNeighbors == 3 then
+		table.insert(points, { x - 1, y - 1, 1, 1, 0 })
+	else
+		table.insert(points, { x - 1, y - 1, 0, 1, 1 })
+	end
 end
 
 love = love or {}
@@ -51,10 +56,7 @@ function love.load()
 	for x = 0, width - 1 do
 		grid[x + 1] = {}
 		for y = 0, height - 1 do
-			grid[x + 1][y + 1] = false
-			if math.random(0, 1) == 1 then
-				grid[x + 1][y + 1] = true
-			end
+			grid[x + 1][y + 1] = math.random() >= 0.5
 		end
 	end
 end
